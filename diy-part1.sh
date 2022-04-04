f9f362f31434db0c4273047bac054b0e7c7464eb
Tang <78092871+tangfenga@users.noreply.github.com>
2022-04-04 10:31:57 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf package/helloworld
+cd package/
+git clone https://github.com/liuran001/openwrt-packages 
---
+++ diy-part2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
