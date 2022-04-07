e63ea2e40feabd2afd7e7f130169befc6bf587a8
Tang <78092871+tangfenga@users.noreply.github.com>
2022-04-07 09:32:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-rm -rf package/helloworld
-cd package/
-git clone https://github.com/liuran001/openwrt-packages 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+rm -rf feeds.conf.default
+
+# Add a feed source
+echo 'src-git packages https://github.com/CCA2878/immortalwrt-packages.git;openwrt-18.06' >>feeds.conf.default
+echo 'src-git luci https://github.com/CCA2878/immortalwrt-luci.git;openwrt-18.06-k5.4' >>feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git' >>feeds.conf.default
+
