36cba100fe3a13a61184b242ec1620c96ff0ed56
Tang <78092871+tangfenga@users.noreply.github.com>
2022-04-12 10:32:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,8 +5,6 @@
 rm -rf feeds.conf.default
 
 # Add a feed source
-echo 'src-git packages https://github.com/CCA2878/immortalwrt-packages.git;openwrt-18.06' >>feeds.conf.default
-echo 'src-git luci https://github.com/CCA2878/immortalwrt-luci.git;openwrt-18.06-k5.4' >>feeds.conf.default
 echo 'src-git routing https://github.com/openwrt/routing.git' >>feeds.conf.default
 echo 'src-git telephony https://github.com/openwrt/telephony.git' >>feeds.conf.default
 
