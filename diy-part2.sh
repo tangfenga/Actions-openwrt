0f8b33ec2433bacdefe5cadf5c119c05c8069f0d
Tang <78092871+tangfenga@users.noreply.github.com>
2022-04-12 11:56:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,8 +3,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
 
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
 # add packages
 cd package/
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
