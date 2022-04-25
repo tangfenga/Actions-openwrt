63bcc3f225a34b3ad9453498aa9c971c6a9e9385
Tang <78092871+tangfenga@users.noreply.github.com>
2022-04-25 14:50:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,5 +6,6 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 # add packages
 cd package/
 git clone https://github.com/liuran001/openwrt-packages
+rm -rf luci-theme-*
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
