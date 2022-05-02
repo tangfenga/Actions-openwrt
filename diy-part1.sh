0890bc1cb44434686638789ec9e2eb1ccee24aca
tangfenga <tang@tnul.top>
2022-05-02 08:59:21 +0800

    增加常用软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-
 # Add a feed source
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
