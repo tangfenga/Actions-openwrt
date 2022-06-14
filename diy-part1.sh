b0c3373c400521000695bada92bdc2b5d1323fd2
tangfenga <tang@tnul.top>
2022-06-14 22:54:22 +0800

    [fix] 更换对应软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,3 +5,5 @@
 
 # Add a feed source
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
