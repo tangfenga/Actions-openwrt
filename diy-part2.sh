0ce08eb8b1c9e27f196cb04acdd0f2548b927517
tangfenga <tang@tnul.top>
2022-06-15 10:57:13 +0800

    [up] 自己clone

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,4 +5,10 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 
 # add packages
 cd package/
-
+mkdir tang
+cd tang
+git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/fw876/helloworld.git
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
+git clone --depth 1 https://github.com/messense/aliyundrive-webdav
