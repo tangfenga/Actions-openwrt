3cbf999ce79ab7ebed90c11fc7b94bdb50f28c1d
tangfenga <tang@tnul.top>
2022-06-15 11:29:05 +0800

    [fix] 换用软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,3 +4,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -5,10 +5,12 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 
 # add packages
 cd package/
-mkdir tang
-cd tang
-git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/fw876/helloworld.git
-git clone --depth=1 https://github.com/vernesong/OpenClash.git
-git clone --depth 1 https://github.com/messense/aliyundrive-webdav
+# mkdir tang
+# cd tang
+# git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
+# git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
+# git clone --depth=1 https://github.com/fw876/helloworld.git
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git
+# git clone --depth 1 https://github.com/messense/aliyundrive-webdav
+# git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
+# git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
