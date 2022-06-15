343da6550123d9182f6bf0862c8f1df946d5faa8
tangfenga <tang@tnul.top>
2022-06-15 10:39:43 +0800

    [fix] 解决依赖问题

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,6 +4,3 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
