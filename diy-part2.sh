179007d30b0ff9a3b2746f816528595883d8db15
tangfenga <tang@tnul.top>
2022-06-28 19:24:46 +0800

    [change] 换用smartdns为adguardhome

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,11 +7,10 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 cd package/
 mkdir tang
 cd tang
-git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/fw876/helloworld.git
 git clone --depth=1 https://github.com/vernesong/OpenClash.git
-git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
 
 #clone alidrive-webdav
