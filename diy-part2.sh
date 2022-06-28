2e65ae9eabc92e95aab3d6df0f8c067a2bbcb94a
tangfenga <tang@tnul.top>
2022-06-28 18:59:27 +0800

    [fix] 感谢大佬@liuran001，部分软件包改用svn

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,12 +5,15 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 
 # add packages
 cd package/
-# mkdir tang
-# cd tang
-# git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
-# git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
-# git clone --depth=1 https://github.com/fw876/helloworld.git
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git
-# git clone --depth 1 https://github.com/messense/aliyundrive-webdav
-# git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
-# git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+mkdir tang
+cd tang
+git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/fw876/helloworld.git
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
+git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+
+#clone alidrive-webdav
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
