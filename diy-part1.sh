d576bb1fbd01ca168f2ee968435ec1a692b4814a
tangfenga <tang@tnul.top>
2022-06-30 09:39:22 +0800

    [fix] 用大佬@liuran001 的软件包解决ssp依赖问题

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,5 +5,4 @@
 
 # Add a feed source
 #sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git liuran001_packages https://github.com/liuran001/openwrt-packages" >> "feeds.conf.default"
--- diy-part2.sh
+++ diy-part2.sh
@@ -5,13 +5,13 @@ sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generat
 
 # add packages
 cd package/
-mkdir tang
-cd tang
-git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/vernesong/OpenClash.git
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+# mkdir tang
+# cd tang
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 
-#clone alidrive-webdav
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
+# #clone alidrive-webdav
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
