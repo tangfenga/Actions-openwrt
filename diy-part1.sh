bc767240e6dfb53023e28464b6895f665ed9137b
tangfenga <tang@tnul.top>
2022-07-08 21:53:37 +0800

    [update] 暂时换用大佬的软件包，更新完成后再更新自己的软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,4 +5,7 @@
 
 # Add a feed source
 #sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
-echo "src-git liuran001_packages https://github.com/liuran001/openwrt-packages" >> "feeds.conf.default"
+# echo "src-git liuran001_packages https://github.com/liuran001/openwrt-packages" >> "feeds.conf.default"
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git pull
