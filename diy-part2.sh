bf5b135c69f1dabab00ed29c5171a05ba58694a2
tangfenga <tang@tnul.top>
2022-07-13 10:54:06 +0800

    [fix] 更改内网地址

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # add packages
 cd package/
